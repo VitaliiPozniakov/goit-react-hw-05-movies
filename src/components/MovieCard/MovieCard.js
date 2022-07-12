ffd3b38f014d7fdd6519319dd7e91a5a57d489ce
VitaliiPozniakov <vertalen89@gmail.com>
2022-07-12 13:29:55 +0300

    1

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,25 +1,19 @@
-import {  Item,
-    Title,
-   
-    CustomLink} from './MovieCard.styled'
+import { Item, Title, CustomLink } from './MovieCard.styled';
 import propTypes from 'prop-types';
 
 function MovieCard({ id, title }) {
+  return <>
+    {title && (<Item>
+      <CustomLink to={`/movies/${id}`}>
+        <Title>{title}</Title>
+      </CustomLink>
+    </Item>)}</>
+  
+}
 
-    return (
-      <Item>
-<CustomLink to={`/movies/${id}`}>
-          <Title>{title && title}</Title>
-     
-       
-          </CustomLink>
-      </Item>
-    );
-  }
-  
-  MovieCard.prototype = {
-    id: propTypes.number,
-    title: propTypes.string,
-  };
-  
-  export default MovieCard;
+MovieCard.prototype = {
+  id: propTypes.number,
+  title: propTypes.string,
+};
+
+export default MovieCard;
