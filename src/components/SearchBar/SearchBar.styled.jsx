3fdbd58ddb7dc6ffabc6b8375c96bca740fbce87
VitaliiPozniakov <vertalen89@gmail.com>
2022-07-30 16:56:53 +0300

    1

--- src/components/SearchBar/SearchBar.styled.jsx
+++ src/components/SearchBar/SearchBar.styled.jsx
@@ -40,7 +40,7 @@ export const Input = styled.input`
 `;
 
 export const Btn = styled.button`
-  padding: 5px 32px;
+  padding: 5px 10px;
   color: black;
   background-color: #ffffffff;
   font-weight: 700;
@@ -51,14 +51,14 @@ export const Btn = styled.button`
   border-radius: 4px;
   border: 1px solid rgba(238, 238, 238, 1);
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
-  width: 150px;
+  width: 100px;
   height: 40px;
   margin-left: auto;
 
   transition: color, background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
 
   @media (min-width: 768px) {
-    width: 100px;
+    width: 150px;
   }
 
   &:hover,
